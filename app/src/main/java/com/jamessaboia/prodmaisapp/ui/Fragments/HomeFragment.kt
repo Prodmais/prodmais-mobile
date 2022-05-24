c7f9f2e1c9bbfcb3e06530af072ca122ac0dbd0b
James Saboia <saboiajamesjr@gmail.com>
2022-05-24 16:23:11 -0400

    adicionando botão logout

--- app/src/main/java/com/jamessaboia/prodmaisapp/ui/Fragments/HomeFragment.kt
+++ app/src/main/java/com/jamessaboia/prodmaisapp/ui/Fragments/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.*
+import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
